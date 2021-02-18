3f669ffc049e9694135ec8d34a8b47ddf157203f
dragon-msi <iamk.vikash@yahoo.com>
2021-02-18 20:52:26 +0530

    SignUp Form modified

--- myblog/userauth/views.py
+++ myblog/userauth/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import SignUpForm
 # Create your views here.
 class UserSignUp(CreateView):
-    form_class=UserCreationForm
+    form_class= SignUpForm
     template_name='registration/signup.html'
     # name the file as "registration"
     success_url=reverse_lazy('login')
